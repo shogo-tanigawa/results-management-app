e6b1255cdf39e7ba290b88c2484179c7bc53f14c
shogo <kurohamu2525@gmail.com>
2023-03-01 21:38:24 +0900

    Finish page-layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new'
   root 'static_pages#top'
+  get '/signup', to: 'users#new'
+
   devise_for :users, controllers: {
     omniauth_callbacks: "omniauth_callbacks"
   }
