0c91df31466aa8f1b19dccebebbcfb660eacf91b
shogo <kurohamu2525@gmail.com>
2023-03-07 00:24:30 +0900

    マイグレーションファイルを修正

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: {
     registrations: 'users/registrations',
-    sessions     : 'users/sessions',
     omniauth_callbacks: 'omniauth_callbacks'
   }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,9 @@
 ActiveRecord::Schema.define(version: 2023_02_27_143804) do
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
+    t.string "name"
+    t.string "email"
+    t.string "password"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -22,7 +23,6 @@ ActiveRecord::Schema.define(version: 2023_02_27_143804) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "provider"
     t.string "uid"
-    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
