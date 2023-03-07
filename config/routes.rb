c5367f5382a60798cb71fbe2933de16561433628
shogo <kurohamu2525@gmail.com>
2023-03-08 00:09:22 +0900

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/new'
   root 'static_pages#top'
-  get '/signup', to: 'users#new'
 
   devise_for :users, controllers: {
-    registrations: 'users/registrations',
     omniauth_callbacks: 'omniauth_callbacks'
   }
+
+  devise_scope :users do
+    get '/users', to: redirect("/users/sign_up")
+  end
 end
