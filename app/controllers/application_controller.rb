034fd0d37271dfc23afe3a8ec582fc5c1f0379b2
shogo <kurohamu2525@gmail.com>
2023-03-08 23:52:13 +0900

    ユーザー編集、name更新されない問題解決

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     added_attrs = [ :name, :email, :password ]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
 end
