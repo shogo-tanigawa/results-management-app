4746145be38e433dd1129cef33ccffefcd5a9c89
shogo <kurohamu2525@gmail.com>
2023-03-19 14:27:10 +0900

    ﾕｰｻﾞｰ一覧に削除ボタンを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
+  include UsersHelper
   before_action :authenticate_user!
-  
+
   def index
     @users = User.all
   end
@@ -8,4 +9,5 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  
 end
