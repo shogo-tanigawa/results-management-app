06f392ab4fd187c5dbc116be0bc47f1c4da31275
shogo <kurohamu2525@gmail.com>
2023-03-20 22:50:04 +0900

    gamesのﾙｰﾃｨﾝｸﾞ設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'games/new'
   root 'static_pages#top'
 
   devise_for :users, controllers: {
     omniauth_callbacks: 'omniauth_callbacks'
   }
   resources :users, :only => [:index, :show]
+  resources :games
 
   devise_scope :users do
     get '/users', to: redirect("/users/sign_up")
